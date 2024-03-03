211be8f5ba2480eeb7d3097a5bbe36063b22f0a3
Webaadhi5E <aadthiya.ah@gmail.com>
2024-03-03 21:10:31 +0530

    New routing add for Login-page signup page and Welcome page

--- My Projects/src/app/app-routing.module.ts
+++ My Projects/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome/welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+  { path: 'welcome', loadChildren: () => import('./welcome/welcome-page/welcome-routing.module').then((m) => m.WelcomeRoutingModule) },
+  { path: 'login', loadChildren: () => import('./login/login-routing.module').then((m) => m.LoginRoutingModule) }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
