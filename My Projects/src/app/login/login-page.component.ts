ee666ea31e8b2fcd3809e93bffce9fe9337096d2
Webaadhi5E <aadthiya.ah@gmail.com>
2024-03-09 16:16:53 +0530

    Login & Sign-up Form Base Init

--- My Projects/src/app/login/login-page.component.ts
+++ My Projects/src/app/login/login-page.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser,faKey } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, CommonModule, FormsModule, ReactiveFormsModule, FontAwesomeModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  @Input() loginPage: string | undefined;
+  public loginForm!: FormGroup;
+  router: any;
+  public showPassword!: boolean;
+  public input: any;
+  public submitted: boolean = false;
+  public faUser = faUser;
+  public faKey = faKey;
 
+
+  constructor(private route: Router) { }
+
+  public get form() { return this.loginForm.controls; }
+  public enterdData: any;
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      emailid: new FormControl('', [Validators.required, Validators.email, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,63}$')]),
+      password: new FormControl('', [Validators.required, Validators.pattern(
+        '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$'
+      )]),
+    })
+  }
+
+  public onSubmit() {
+    this.enterdData = JSON.stringify(this.loginForm.value);
+    this.checkLocalData(JSON.parse(this.enterdData));
+  }
+
+  public checkLocalData(data: any) {
+    let localdata: any = JSON.parse(localStorage.getItem('mykey') || '{}');
+    console.log(data.emailid);
+    console.log(data.password);
+    if (localdata && localdata.email === data.emailid) {
+      if (localdata.password === data.password) {
+        this.route.navigate(["/main"])
+      }
+    }
+    else {
+      alert("enter valid emailid & Password")
+    }
+  }
+
+  public toggleShow() {
+    this.showPassword = !this.showPassword;
+    this.input.type = this.showPassword ? 'text' : 'password';
+  }
 }
