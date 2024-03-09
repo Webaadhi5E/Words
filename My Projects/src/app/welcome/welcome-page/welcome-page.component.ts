013b72e0bc74049d215a43bfeeae2b03d5903ed4
Webaadhi5E <aadthiya.ah@gmail.com>
2024-03-09 20:06:05 +0530

    Routers Enhancement and New Components added changes

--- My Projects/src/app/welcome/welcome-page/welcome-page.component.ts
+++ My Projects/src/app/welcome/welcome-page/welcome-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from 'src/app/common/header/header.component';
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,RouterOutlet],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.scss'
 })
