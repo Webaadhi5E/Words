5514bbab5539ee041929d76b358775c39879338a
Webaadhi5E <aadthiya.ah@gmail.com>
2024-04-05 06:47:42 +0530

    First Unit Test Done

--- My Projects/src/app/page/tables/multiplication-tables/multiplication-tables.component.spec.ts
+++ My Projects/src/app/page/tables/multiplication-tables/multiplication-tables.component.spec.ts
@@ -1,17 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MultiplicationTablesComponent } from './multiplication-tables.component';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 describe('MultiplicationTablesComponent', () => {
   let component: MultiplicationTablesComponent;
   let fixture: ComponentFixture<MultiplicationTablesComponent>;
+  let mockActivatedRoute: ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MultiplicationTablesComponent]
+      imports: [MultiplicationTablesComponent, RouterModule.forRoot([])],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute, // Provide the mock
+        },
+      ],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(MultiplicationTablesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
