b2bdffbbbedb6b879ea9ba10d9d809ef95d673a3
Webaadhi5E <143205525+Webaadhi5E@users.noreply.github.com>
2024-04-11 21:40:32 +0530

    ENhancement

--- My Projects/src/app/common/header/header.component.ts
+++ My Projects/src/app/common/header/header.component.ts
@@ -1,15 +1,17 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive,NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
-public isOpen: boolean = false;
+  @Input() scroll: number = 0;
+  public isOpen: boolean = false;
+
 }
