d1f561ecb0dd86f38c138332324afc128e5bbcc9
Webaadhi5E <143205525+Webaadhi5E@users.noreply.github.com>
2024-05-06 06:08:33 +0530

    new route path update

--- My Projects/src/app/routes/project-routing.module.ts
+++ My Projects/src/app/routes/project-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserDetailComponent } from '../page/user-project/user-detail.component';
 import { ColorPalletsComponent } from '../page/color-pallets/color-pallets.component';
 import { MultiplicationTablesComponent } from '../page/tables/multiplication-tables/multiplication-tables.component';
+import { CrudAppComponent } from '../page/crud-app/crud-app.component';
 
 const routes: Routes = [
   { path: 'user-details', component: UserDetailComponent, title: 'Users-Detail' },
   { path: 'color-pallet', component: ColorPalletsComponent, title: "Colors-Pallet" },
-  { path: 'tables', component: MultiplicationTablesComponent, title: 'Multiplication-Tables' }
+  { path: 'tables', component: MultiplicationTablesComponent, title: 'Multiplication-Tables' },
+  { path: 'crud', component: CrudAppComponent, title: 'CRUD-Project' },
 ];
 
 @NgModule({
