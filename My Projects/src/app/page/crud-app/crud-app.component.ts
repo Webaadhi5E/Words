97cf8879e9a890a1043de62e49df3478c1b983be
Webaadhi5E <143205525+Webaadhi5E@users.noreply.github.com>
2024-05-13 21:40:18 +0530

    CURD-integration-changes

--- My Projects/src/app/page/crud-app/crud-app.component.ts
+++ My Projects/src/app/page/crud-app/crud-app.component.ts
@@ -53,11 +53,30 @@ export class CrudAppComponent {
   ];
   public displayedUserDetails: Array<any> = [];
   public newName: string = "";
+  public temIndex: any;
   constructor() {
     this.displayedUserDetails = this.userObj
   }
 
   public renameCandidate() {
+    this.displayedUserDetails[this.temIndex].name = this.newName;
+  }
 
+  public changeName(name: any, i: any) {
+    this.newName = name;
+    this.temIndex = i;
+  }
+
+  public searchItem(e: any) {
+    if (!e.target.value) {
+      return (this.displayedUserDetails = this.userObj);
+    } else {
+      this.displayedUserDetails = [];
+      this.userObj.filter((items: any) => {
+        if (items.name.toLowerCase() === e?.target.value.toLowerCase()) {
+          this.displayedUserDetails.push(items);
+        }
+      })
+    }
   }
 }
