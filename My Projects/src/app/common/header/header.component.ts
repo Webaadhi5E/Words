cb0f8db4cc4e1d62df0c000b21ab455bc083ab41
Webaadhi5E <143205525+Webaadhi5E@users.noreply.github.com>
2024-05-17 21:55:49 +0530

    logout redirect to login page changes

--- My Projects/src/app/common/header/header.component.ts
+++ My Projects/src/app/common/header/header.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule, NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Route, Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive,NgClass],
+  imports: [CommonModule, RouterLink, RouterLinkActive, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -14,4 +14,12 @@ export class HeaderComponent {
   @Input() scroll: number = 0;
   public isOpen: boolean = false;
 
+  constructor(private router: Router) {
+
+  }
+
+  public logout() {
+    localStorage.removeItem('mykey');
+    this.router.navigate(['login']);
+  }
 }
