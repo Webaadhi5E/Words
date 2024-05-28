80be96ce58eeb6de335199368a08c34bb919809a
Webaadhi5E <143205525+Webaadhi5E@users.noreply.github.com>
2024-05-28 07:00:38 +0530

    new Interface update changes

---
+++ My Projects/src/app/page/student-details/student.interface.ts
@@ -0,0 +1,6 @@
+export const SEARCHDROPDOWN: any = [
+  { key: 'studentName', value: 'Student Name' },
+  { key: 'location', value: 'Location' },
+  { key: 'studentRegistration', value: 'Student Registration' },
+  { key: 'department', value: 'Student Department' },
+]
