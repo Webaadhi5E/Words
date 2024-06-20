323f088df0d654de70eae2a31b8f19a1bec1e2d4
Webaadhi5E <143205525+Webaadhi5E@users.noreply.github.com>
2024-06-20 06:58:07 +0530

    new popup updates

--- My Projects/src/app/common-utils/common-utlis-popup/common-utlis-popup.component.ts
+++ My Projects/src/app/common-utils/common-utlis-popup/common-utlis-popup.component.ts
@@ -16,8 +16,13 @@ export class CommonUtlisPopupComponent {
   @Output() emitForm: EventEmitter<any> = new EventEmitter();
   public userPopup: any;
   public addUserForm!: FormGroup;
+  public newCandidateForm!: FormGroup;
+  formBuilder: any;
+  http: any;
+  public openCandidatePopup: any;
   constructor() {
-    this.prepareForm()
+    this.prepareForm();
+    this.prepareFormObj();
   }
 
   public openModalPopup() {
@@ -37,5 +42,49 @@ export class CommonUtlisPopupComponent {
   public onSubmitForm() {
     this.emitForm.emit(this.addUserForm.value);
     this.addUserForm.reset();
+  }
+
+  // New Candidate added Form
+
+  public prepareFormObj() {
+    this.newCandidateForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      dob: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required), // Apply email validator here
+      rollno: new FormControl('', Validators.required),
+      standard: new FormControl('', Validators.required),
+      contact: new FormControl('', Validators.required),
+      gender: new FormControl('', Validators.required),
+    });
+  }
+
+  public submitUserForm() {
+    if (this.newCandidateForm.valid) {
+      const formData = this.newCandidateForm.value;
+      // this.http.post('https://66338431f7d50bbd9b49a5cf.mockapi.io/api/v1/students123', formData)
+      //   .subscribe(
+      //     (response) => {
+      //       console.log('Post success:', response);
+      //     },
+      //     (error) => {
+      //       console.error('Post error:', error);
+      //     }
+      //   );
+      this.http.post('https://66338431f7d50bbd9b49a5cf.mockapi.io/api/v1/students123', formData).subscribe((resp: any) => {
+        console.log('Post success', resp);
+      }, (error: any) => {
+        console.log('Post Error', error);
+
+      })
+    } else {
+      console.error('Form is invalid');
+    }
+    this.newCandidateForm.reset();
+  }
+
+  public openCandidateModalPopup() {
+    this.openCandidatePopup = new bootstrap.Modal("#studentModal");
+    this.openCandidatePopup.show();
   }
 }
