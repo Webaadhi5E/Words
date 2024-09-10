77d9edffcafbc26219053f17390c0558562cbb78
Webaadhi5E <143205525+Webaadhi5E@users.noreply.github.com>
2024-09-10 15:32:17 +0530

    un used code removed

--- My Projects/src/app/page/to-do-list/to-do-list.component.ts
+++ My Projects/src/app/page/to-do-list/to-do-list.component.ts
@@ -1,6 +1,5 @@
 import { NgClass } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from 'src/app/common/header/header.component';
 import { HoverHighlightDirective } from 'src/app/hover-highlight.directive';
@@ -13,32 +12,5 @@ import { HoverHighlightDirective } from 'src/app/hover-highlight.directive';
   styleUrl: './to-do-list.component.scss'
 })
 export class ToDoListComponent {
-
-  public title = signal<string>("Angular Interview Prepare with all Concepts");
-  public isTitleReady: boolean = true;
-  public subTitle: string = "";
-  public mainProductList: any;
-  public displayProductsList: Array<any> = [];
-  protected http = inject(HttpClient);
-  constructor() {
-    // this.displayProductsList = Array.from(Array(10).keys());
-    this.getProductData()
-  }
-  public displayDynamicTitle() {
-    this.isTitleReady = !this.isTitleReady;
-  }
-
-  public getProductData() {
-    this.http.get('https://dummyjson.com/products').subscribe((data: any) => {
-      this.getProductsDataResponse(data)
-    })
-  }
-
-  public getProductsDataResponse(res: any) {
-    this.mainProductList = res.products;
-    this.displayProductsList = this.mainProductList;
-    console.log(this.displayProductsList);
-  }
-
 
 }
