7997bde54e0e1f3a12eb6ee99a531ad151b92349
Webaadhi5E <aadthiya.ah@gmail.com>
2024-10-13 13:20:04 +0530

    new component added for TM

---
+++ My Projects/src/app/page/tmdashdev/tmdashdev.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tmdashdev',
+  standalone: true,
+  imports: [],
+  templateUrl: './tmdashdev.component.html',
+  styleUrl: './tmdashdev.component.scss'
+})
+export class TMdashdevComponent {
+
+}
